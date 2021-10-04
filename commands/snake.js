926ff16193d3566e62ae94729f8a4382115ca3b5
thanhgaming5550 <vuilachinh18122006@gmail.com>
2021-10-04 21:12:14 +0700

    2.4.5

--- commands/snake.js
+++ commands/snake.js
@@ -1,4 +1,4 @@
-const djsGames = require('djs-games')
+const { Snake } = require('djs-games')
 const Discord = require("discord.js");
 
 module.exports = {
@@ -9,7 +9,17 @@ module.exports = {
     const menu = require('../modules/menu.js')
         const cmdlog = new menu.cmdlog()
         cmdlog.log(message)
-    const SnakeGame = new djsGames.SnakeGame()
-    SnakeGame.startGame(message)
+        const game = new Snake({
+            message: message,
+            buttons: true, // If you want to use buttons || False if you want to use reactions
+            snake: '🟩',
+            apple: '🍎',
+            embedColor: 'RANDOM',
+            leftButton: '◀',
+            rightButton: '▶',
+            upButton: '▲',
+            downButton: '▼',
+          })
+          game.start()
     }
 }
--- commands/type.js
+++ commands/type.js
@@ -1,4 +1,4 @@
-const djsGames = require('djs-games')
+const { FastType } = require('djs-games')
 const Discord = require("discord.js");
 
 module.exports = {
@@ -7,9 +7,12 @@ module.exports = {
 
     async run (client, message, args) {
     const menu = require('../modules/menu.js')
-        const cmdlog = new menu.cmdlog()
-        cmdlog.log(message)
-    const FastTyper = new djsGames.FastTyper()
-    FastTyper.startGame(message)
+    const cmdlog = new menu.cmdlog()
+    cmdlog.log(message)
+    
+    const game = new FastType({
+        message: message,
+      })
+      game.start()
     }
 }
