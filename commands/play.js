8ed84a07f48297a45d6de1a884a0601662cb8e95
thanhgaming5550 <vuilachinh18122006@gmail.com>
2021-10-06 21:44:26 +0700

    old

--- commands/play.js
+++ commands/play.js
@@ -13,27 +13,21 @@ module.exports = {
         if (!message.member.voice.channel) return message.channel.send("❌ | **Bạn phải ở trong một kênh nói!**");
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(":x: | **Bạn phải ở cùng kênh nói với Bot!**"); 
 
-        const searchResult = await player.search(args.join(" "), {
-            requestedBy: message.user,
-            //searchEngine: QueryType.AUTO
-        });
-        if (!searchResult || !searchResult.tracks.length) {
-            return void message.channel.send("❌ | Không tìm thấy nhạc/video hoặc đã xảy ra lỗi khi tìm kiếm");
-        }
-        //if (!searchResult.playlist && !searchResult.tracks[0]) return message.channel.send("❌ | Không tìm thấy nhạc/video");
-        
         const queue = await player.createQueue(message.guild, {
             metadata: message.channel
         });
-        
         try {
             if (!queue.connection) await queue.connect(message.member.voice.channel);
         } catch {
-            void player.deleteQueue(message.guildId);
-            return void message.channel.send("❌ | Đã xảy ra lỗi khi tham gia kênh nói!");
+            queue.destroy();
+            return message.channel.send("❌ | Đã xảy ra lỗi khi tham gia kênh nói!");
         }
-        
-        await message.channel.send(`⏱ | Đang tải ${searchResult.playlist ? "danh sách phát" : "bài nhạc"}...` );
+        const searchResult = await player.search(args.join(" "), {
+            requestedBy: message.user,
+            searchEngine: QueryType.AUTO
+        })
+        if (!searchResult || !searchResult.tracks.length) return message.channel.send("❌ | Không tìm thấy nhạc/video hoặc đã xảy ra lỗi khi tìm kiếm");
+        message.channel.send(`⏱ | Đang tải ${searchResult.playlist ? "danh sách phát" : "bài nhạc"}...` );
         if (searchResult.playlist) {queue.addTracks(searchResult.tracks)} else {queue.addTrack(searchResult.tracks[0])};
         if (!queue.playing) await queue.play();
     }
