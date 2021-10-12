13aa751990e6cae18cdf1b6fc5e2644cfe3cf452
thanhgaming5550 <vuilachinh18122006@gmail.com>
2021-10-12 11:47:16 +0700

    2.6.0

---
+++ commands/wordsnack.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const { DiscordTogether } = require('discord-together');
+
+module.exports = {
+    name: "wordsnack",
+    description: "play wordsnack",
+
+    async run (client, message, args){
+        const menu = require('../modules/menu.js')
+        const cmdlog = new menu.cmdlog()
+        cmdlog.log(message)
+        client.discordTogether = new DiscordTogether(client);
+        if(message.member.voice.channel) {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'wordsnack').then(async invite => {
+                return message.channel.send(`BẤM VÀO **LIÊN KẾT** ĐÂY : ${invite.code}`)  
+            });
+        } else message.channel.send(`Bạn phải ở trong 1 kênh nói thoại`);
+    }
+}
