404e6ad748b0fa2daec6b3ad38b64d93eec3c3c7
thanhgaming5550 <62001770+thanhgaming5550@users.noreply.github.com>
2022-03-03 16:37:03 +0700

    after leave

--- index.js
+++ index.js
@@ -100,14 +100,21 @@ const { YtDlpPlugin } = require("@distube/yt-dlp");
             //    type: "STREAMING",
             //    url: "https://www.twitch.tv/thanhgaming5550"
             //})
+            /*
             console.log('=========================================================================================================');
-            client.guilds.cache.forEach(async guild => {
-                {
-                    console.log(`Bot đang ở ${guild.name} (${guild.id}) có ${guild.memberCount} người`);
-                    //await guild.leave()
-                }
-            })
+            client.guilds.cache.get("") // Leave Guild
+                .leave()
+                .then((g) => {console.log("Leaved "+g)})
+                
+            client.guilds.cache.forEach(async guild => { // Check Guild and Member
+                    {
+                        console.log(`Bot đang ở ${guild.name} (${guild.id}) có ${guild.memberCount} người`);
+                        //await guild.leave()
+                    }
+                })
+            
             console.log('=========================================================================================================');
+            */    
         console.log(colors.bold(colors.green(`Logged in as ${client.user.tag}!`)));
         console.log(colors.green(`Online`));
         console.log(`Bot hiện đang ở ${client.guilds.cache.size} máy chủ, theo dõi ${client.channels.cache.size} kênh và phục vụ ${cmdcount} lệnh chữ và ${slscount} lệnh gạch chéo cho ${client.users.cache.size} người dùng`);
