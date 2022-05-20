4313b5bff7abab097c44dfc7245e01eab975cc00
thanhgaming5550 <62001770+thanhgaming5550@users.noreply.github.com>
2022-05-20 07:42:27 +0700

    2.13.4

--- commands/invite.js
+++ commands/invite.js
@@ -8,14 +8,13 @@ module.exports = {
             
         const embed = new Discord.MessageEmbed()
             .setColor("RANDOM")
-            .setTitle(`**-> NHẤN VÀO ĐÂY ĐỂ MỜI <-**`)
+            .setTitle(`**BOT THIEU NANG**`)
             .setThumbnail('https://i.imgur.com/jUcU9ac.png')
             .setDescription(`Bot Thieu Nang - Một con Discord Bot với rất nhiều chức năng thú vị, như xem video YouTube, chơi game với bạn bè (có giao diện tương tác), nghe nhạc, quay ngẫu nhiên, nói thay hộ bạn, nói chuyện với SimSimi,... ngay tại Discord mà không phải đi bất cứ đâu!\n\n**TẤT CẢ ĐỀU MIỄN PHÍ 100%, KHÔNG BAO GIỜ DÙNG TIỀN ĐỂ DÙNG, MIỄN PHÍ MÃI MÃI!**
                 
             **[MỜI BOT NGAY!](https://discord.com/oauth2/authorize?client_id=848103224854315018&permissions=412320525376&scope=applications.commands+bot)**
                 
             - Server Support: *Dùng lệnh \`/>support\`*`)
-            .setURL(`https://discord.com/oauth2/authorize?client_id=848103224854315018&permissions=4294967287&scope=applications.commands%20bot`)
             .setTimestamp()
             .setFooter({text: 'Một sản phẩm đầu tay của thanhgaming5550'});
         message.reply({embeds : [embed]});
